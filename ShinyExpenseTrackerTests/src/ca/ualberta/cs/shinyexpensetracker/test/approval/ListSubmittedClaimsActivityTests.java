8ef16b926db2a514456905777154d5d751073231
Adam Ford <adam.ford@greatbigsolutions.com>
2015-02-11 21:23:17 -0700

    Add Approval test fixture

---
+++ ShinyExpenseTrackerTests/src/ca/ualberta/cs/shinyexpensetracker/test/approval/ListSubmittedClaimsActivityTests.java
@@ -0,0 +1,16 @@
+package ca.ualberta.cs.shinyexpensetracker.test.approval;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class ListSubmittedClaimsActivityTests extends
+		ActivityInstrumentationTestCase2<T> {
+
+	public ListSubmittedClaimsActivityTests(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}

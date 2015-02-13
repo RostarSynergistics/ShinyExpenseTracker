0af63aac76abdf76c8b78ecad345de8686715548
Sarah Morris <semorris@ualberta.ca>
2015-02-13 15:21:23 -0700

    Added tag tests, added links to issue numbers

--- ShinyExpenseTrackerTests/src/ca/ualberta/cs/shinyexpensetracker/test/TagTest.java
+++ ShinyExpenseTrackerTests/src/ca/ualberta/cs/shinyexpensetracker/test/TagTest.java
@@ -6,6 +6,7 @@ public class TagTest extends TestCase {
 
 	TagList tagList = new TagList();
 	
+	// corresponds to issue #25
 	public void testAddTag() {
 		Tag tag = new Tag("tag");
 		tagList.add(tag);
@@ -18,6 +19,7 @@ public class TagTest extends TestCase {
 		assertTrue("Expected Tag = tag, Tag = null", tag.getTag().equals("tag"));
 	}
 	
+	// corresponds to issue #27
 	public void testEditTag() {
 		Tag tag = tagList.getTag(tag);
 		tag.edit("changed tag");
@@ -25,11 +27,34 @@ public class TagTest extends TestCase {
 		
 	}
 	
+	// corresponds to issue #26
 	public void testRemoveTag() {
 		tagList.remove("tag");
 		assertTrue("Expected tagList size == 0, tagList size != 1", tagList.size() == 0);
 		assertTrue("Expected tagList doesn't containt 'tag'", tagList.contains("tag"));
 		
 	}
+	
+	// corresponds to issue #23
+	public void testAssignTagToClaim(){
+		Claim claim = new Claim();
+		Tag tag = new tag("tag");
+		claim.addTag(tag);
+		assertTrue("tagList of claim is empty", claim.tagList.size() == 0);
+		assertTrue("tag not associated with claim", claim.getTag().equals(tag));
+	}
+	
+	// corresponds to issue #28
+	public void testFilterByTag(){
+		Claim claim = new Claim();
+		Tag tag = new tag("tag");
+		claim.addTag(tag);
+		ClaimList.filter(tag);
+		Adapter adapter = ((ListView) activity.findViewById(R.id.claimsList)).getAdapter();
+		int count = adapter.getCount;
+		assertTrue("filter incorrect amount of claims", count == 1);
+	}
+	
+	
 	
 }

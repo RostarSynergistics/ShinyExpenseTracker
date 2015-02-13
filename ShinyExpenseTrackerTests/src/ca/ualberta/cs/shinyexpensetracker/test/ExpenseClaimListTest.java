e52b2c6f7779ed6193dc71f6d32079adc650cee0
Adam Ford <adam.ford@greatbigsolutions.com>
2015-02-12 22:55:42 -0700

    Add claim for getting submitted claims

--- ShinyExpenseTrackerTests/src/ca/ualberta/cs/shinyexpensetracker/test/ExpenseClaimListTest.java
+++ ShinyExpenseTrackerTests/src/ca/ualberta/cs/shinyexpensetracker/test/ExpenseClaimListTest.java
@@ -6,13 +6,13 @@ import junit.framework.TestCase;
 
 public class ExpenseClaimListTest extends TestCase {
 	public void testEmptyClaimList(){
-		ExpenseClaimList expenseClaimList = new expenseClaimList();
+		ExpenseClaimList expenseClaimList = new ExpenseClaimList();
     	Collection<ExpenseClaim> claims = expenseClaimList.getClaims();
     	assertTrue("Empty Expense Claim List", claims.size() == 0);
 	}
 	
 	public void testAddClaimToClaimList(){
-		ExpenseClaimList expenseClaimList = new expenseClaimList();
+		ExpenseClaimList expenseClaimList = new ExpenseClaimList();
 		ExpenseClaim claim = new ExpenseClaim("name", new Date(123456), new Date(123457));
 		
 		expenseClaimList.add(claim);
@@ -24,7 +24,7 @@ public class ExpenseClaimListTest extends TestCase {
 	}
 	
 	public void testRemoveClaimFromList(){
-		ExpenseClaimList expenseClaimList = new expenseClaimList();
+		ExpenseClaimList expenseClaimList = new ExpenseClaimList();
 		ExpenseClaim claim = new ExpenseClaim("name", new Date(123456), new Date(123457));
 		
 		expenseClaimList.add(claim);
@@ -35,7 +35,7 @@ public class ExpenseClaimListTest extends TestCase {
 	}
 	
 	public void testEditClaimInList(){
-		ExpenseClaimList expenseClaimList = new expenseClaimList();
+		ExpenseClaimList expenseClaimList = new ExpenseClaimList();
 		ExpenseClaim claim = new ExpenseClaim("name", new Date(123456), new Date(123457));
 		
 		expenseClaimList.add(claim);
@@ -44,5 +44,20 @@ public class ExpenseClaimListTest extends TestCase {
 		retrievedClaim.edit();
 		
 		assertFalse("retrieved claim is not equal to the inserted claim", claim.equals(retrievedClaim));
+	}
+	
+	public void testGetSubmittedClaims() {
+		ExpenseClaimList expenseClaimList = new ExpenseClaimList();
+		ExpenseClaim claim1 = new ExpenseClaim("in_progress", new Date(123456), new Date(123457));
+		expenseClaimList.add(claim1);
+		ExpenseClaim claim2 = new ExpenseClaim("submitted", new Date(123456), new Date(123457));
+		expenseClaimList.add(claim2);
+		ExpenseClaim claim3 = new ExpenseClaim("approved", new Date(123456), new Date(123457));
+		expenseClaimList.add(claim3);
+		
+		ArrayList<ExpenseClaim> retrievedClaims = expenseClaimList.getSubmitted();
+		
+		assertTrue("More claims than expected", retrievedClaims.size() == 1);
+		assertTrue("Claim retrieved not the submitted one", retrievedClaims[0].status == "submitted");
 	}
 }

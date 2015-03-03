f4249bf73b18df16ed6c56d8c6a0f905f890ab20
Sarah Morris <semorris@ualberta.ca>
2015-03-03 10:14:39 -0700

    Implemented datePickerDialog feature to input date in ExpenseItem

--- ShinyExpenseTracker/src/ca/ualberta/cs/shinyexpensetracker/ExpenseItemActivity.java
+++ ShinyExpenseTracker/src/ca/ualberta/cs/shinyexpensetracker/ExpenseItemActivity.java
@@ -1,22 +1,69 @@
 package ca.ualberta.cs.shinyexpensetracker;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 
-public class ExpenseItemActivity extends Activity {
+public class ExpenseItemActivity extends Activity implements OnClickListener{
 
+	 //UI References
+    private EditText date;
+    
+    private DatePickerDialog datePickerDialog;
+    
+    private SimpleDateFormat dateFormatter;
+    
+    private void findViewsById() {
+    	date = (EditText) findViewById(R.id.dateEditText);
+        date.setInputType(InputType.TYPE_NULL);
+        date.requestFocus();
+    }
+ 
+    private void setDateTimeField() {
+        date.setOnClickListener(this);
+        date.setOnClickListener(this);
+        
+        Calendar newCalendar = Calendar.getInstance();
+        
+        datePickerDialog = new DatePickerDialog(this, new OnDateSetListener() {
+ 
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                date.setText(dateFormatter.format(newDate.getTime()));
+            }
+ 
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+        
+    }
+    
+    
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_expense_item);
+		
+		dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        
+        findViewsById();
+        
+        setDateTimeField();
 	}
 
 	@Override
@@ -25,6 +72,13 @@ public class ExpenseItemActivity extends Activity {
 		getMenuInflater().inflate(R.menu.expense_item, menu);
 		return true;
 	}
+	
+    @Override
+    public void onClick(View view) {
+        if(view == date) {
+            datePickerDialog.show();
+        }
+    }
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {

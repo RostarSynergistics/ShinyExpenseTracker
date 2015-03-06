314c6e133d6b5eeaac3d41276429e5d4e63bb465
Tristan Meleshko <tmeleshk@ualberta.ca>
2015-03-05 18:39:23 -0700

    View set up and is set to main view.

---
+++ ShinyExpenseTracker/src/ca/ualberta/cs/shinyexpensetracker/views/ExpenseClaimsView.java
@@ -0,0 +1,35 @@
+package ca.ualberta.cs.shinyexpensetracker.views;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import ca.ualberta.cs.shinyexpensetracker.R;
+
+public class ExpenseClaimsView extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_expense_claims_view);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.expense_claims_view, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}

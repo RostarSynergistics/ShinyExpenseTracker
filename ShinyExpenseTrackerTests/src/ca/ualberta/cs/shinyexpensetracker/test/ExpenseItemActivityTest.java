0c9e53f67d9819646f275a9b2517d9f75d84d2b2
Sarah Morris <semorris@ualberta.ca>
2015-03-10 16:04:41 -0600

    Added license header to top of file

--- ShinyExpenseTrackerTests/src/ca/ualberta/cs/shinyexpensetracker/test/ExpenseItemActivityTest.java
+++ ShinyExpenseTrackerTests/src/ca/ualberta/cs/shinyexpensetracker/test/ExpenseItemActivityTest.java
@@ -1,3 +1,28 @@
+/**
+ *  Copyright (C) 2015  github.com/RostarSynergistics
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Covers Issue 5
+ * Things to implement: saving of an expenseItem to an expenseClaim
+ * @author Sarah Morris
+ * @version 1.0
+ * @since 2015-03-09
+ *
+ * Tests ExpenseItemActivity 
+ */
+
 package ca.ualberta.cs.shinyexpensetracker.test;
 
 import java.lang.reflect.Method;

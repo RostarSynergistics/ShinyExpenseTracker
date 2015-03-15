5cda57d1031c2f44724a38c6921f36ce2fac5a55
ramish94 <ramish@ualberta.ca>
2015-03-15 14:27:26 -0600

    - Added DestinationList.java file - Modified AddDestinationActivty to add Destinations to DestinationList

--- ShinyExpenseTracker/src/ca/ualberta/cs/shinyexpensetracker/AddDestinationActivity.java
+++ ShinyExpenseTracker/src/ca/ualberta/cs/shinyexpensetracker/AddDestinationActivity.java
@@ -1,9 +1,11 @@
 package ca.ualberta.cs.shinyexpensetracker;
 
 import ca.ualberta.cs.shinyexpensetracker.models.Destination;
+import ca.ualberta.cs.shinyexpensetracker.models.DestinationList;
 import ca.ualberta.cs.shinyexpensetracker.models.ExpenseClaim;
 import ca.ualberta.cs.shinyexpensetracker.models.Tag;
 import ca.ualberta.cs.shinyexpensetracker.ExpenseClaimController;
+import android.net.NetworkInfo.DetailedState;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -14,6 +16,8 @@ public class AddDestinationActivity extends Activity {
 	
 	private EditText destinationEditText;
 	private EditText reasonForTravelEditText;
+	private ExpenseClaimController ecc;
+	private DestinationList destList;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,8 @@ public class AddDestinationActivity extends Activity {
 		setContentView(R.layout.activity_add_destination);
 		
 		findViewsById();
+		ecc = ExpenseClaimController.getInstance();
+		destList = new DestinationList();
 	}
 
 	@Override
@@ -35,16 +41,16 @@ public class AddDestinationActivity extends Activity {
 		reasonForTravelEditText = (EditText) findViewById(R.id.reasonEditText);
 	}
 	
-	public boolean addDestination() {
+	public boolean addNewDestination() {
 		
 		String dest = "";
 		String reason = "";
-		ExpenseClaimController.getInstance();
 		
 		dest = destinationEditText.getText().toString();
 		reason = reasonForTravelEditText.getText().toString();
 		
 		Destination destination = new Destination(dest, reason);
+		destList.addDestination(destination);
 		
 		//Still needs to be implemented: Add Destination to ExpenseClaim
 		

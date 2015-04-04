1da2e5ba8454ae8bc5d754d86778b868553e69a4
ramish94 <ramish@ualberta.ca>
2015-04-03 19:50:26 -0600

    Added a more detailed test

--- ShinyExpenseTrackerTests/src/ca/ualberta/cs/shinyexpensetracker/test/ESClaimManagerTest.java
+++ ShinyExpenseTrackerTests/src/ca/ualberta/cs/shinyexpensetracker/test/ESClaimManagerTest.java
@@ -12,20 +12,22 @@ import ca.ualberta.cs.shinyexpensetracker.models.ExpenseClaim.Status;
 import ca.ualberta.cs.shinyexpensetracker.models.ExpenseItem.Category;
 import ca.ualberta.cs.shinyexpensetracker.models.ExpenseItem.Currency;
 import ca.ualberta.cs.shinyexpensetracker.models.ExpenseItem;
+import ca.ualberta.cs.shinyexpensetracker.models.Tag;
+import ca.ualberta.cs.shinyexpensetracker.models.TagList;
 import ca.ualberta.cs.shinyexpensetracker.test.mocks.MockExpenseClaimListPersister;
 
 public class ESClaimManagerTest extends TestCase {
 	
 	ESClaimManager manager = new ESClaimManager();
-	ExpenseClaimController controller;
-	Destination destination = new Destination("Hawaii", "Vacation");
+	Destination destination = new Destination("Vancouver", "Vacation");
+	TagList tagList = new TagList();
+	Tag exTag = new Tag("Example");
 	ExpenseItem item = new ExpenseItem("Flight to YEG", null, Category.AIR_FARE, BigDecimal.valueOf(1000), Currency.CAD);
 	
 	
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		controller = new ExpenseClaimController(new MockExpenseClaimListPersister());
 	}
 	
 	/**
@@ -34,18 +36,27 @@ public class ESClaimManagerTest extends TestCase {
 	 * @throws IOException
 	 */
 	public void testAddClaim() throws IllegalStateException, IOException {
-		ExpenseClaim claim = new ExpenseClaim("Rajan sucks", null, null, Status.IN_PROGRESS, null, "1a");
+		tagList.addTag(exTag);
+		ExpenseClaim claim = new ExpenseClaim("BC trip", null, null, Status.IN_PROGRESS, tagList, "1a");
 		claim.addDestination(destination);
 		claim.addExpense(item);
-		controller.addExpenseClaim(claim);
 		manager.insertClaim(claim);
 		
 		assertEquals("Expense claim names are not equal", manager.getClaim("1a").getName(), 
-				"Rajan sucks");
-		assertEquals("Expense claim destinations are not equal", manager.getClaim("1a").getDestination(0).getName(), 
-				"Hawaii");
-		assertEquals("Expense claim destinations are not equal", manager.getClaim("1a").getDestination(0).getReasonForTravel(), 
+				"BC trip");
+		
+		assertEquals("Expense claim destination names are not equal", manager.getClaim("1a").getDestination(0).getName(), 
+				"Vancouver");
+		
+		assertEquals("Expense claim destination reasons for travel are not equal", manager.getClaim("1a").getDestination(0).getReasonForTravel(), 
 				"Vacation");
+		
+		assertEquals("Expense claim status is not IN PROGRESS", manager.getClaim("1a").getStatus(),
+				Status.IN_PROGRESS);
+		
+		assertEquals("Expense claim tag is not Example", manager.getClaim("1a").getTagList().getTagById(0),
+				exTag);
+		
 		assertEquals("Expense claim items are not equal", manager.getClaim("1a").getExpense(0), 
 				item);
 	}
@@ -54,6 +65,7 @@ public class ESClaimManagerTest extends TestCase {
 	 * Test whether or not ElasticSearch deletes a selected claim
 	 * @throws IOException
 	 */
+	
 	public void testDeleteClaim() throws IOException {
 		manager.deleteClaim("1a");
 		assertNull("Expense claim at index 1a is not null", manager.getClaim("1a"));

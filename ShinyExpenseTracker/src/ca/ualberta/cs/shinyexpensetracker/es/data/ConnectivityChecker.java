ed4b0503e5863320346f3ab50a111878ef10acfd
ramish94 <ramish@ualberta.ca>
2015-04-03 18:56:00 -0600

    Added a Connectivity Checker class

---
+++ ShinyExpenseTracker/src/ca/ualberta/cs/shinyexpensetracker/es/data/ConnectivityChecker.java
@@ -0,0 +1,24 @@
+// Code taken from: http://stackoverflow.com/questions/4238921/detect-whether-there-is-an-internet-connection-available-on-android
+// Apr 3rd, 2015
+
+package ca.ualberta.cs.shinyexpensetracker.es.data;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class ConnectivityChecker {
+	
+	/**
+	 * Checks whether or not the device is connected to the internet. Returns appropriate booleans
+	 * @param context
+	 * @return boolean
+	 */
+	@SuppressWarnings("unused")
+	private boolean isNetworkAvailable(Context context) {
+	    ConnectivityManager connectivityManager 
+	          = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+	    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+	    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+	}
+}

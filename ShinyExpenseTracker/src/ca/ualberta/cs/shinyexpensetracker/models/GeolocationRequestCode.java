696c1354850932301dd41dc1ed911c7023f75f01
oleyniko <oleyniko@ualberta.ca>
2015-04-06 11:38:57 -0600

    I always for get to include new files

---
+++ ShinyExpenseTracker/src/ca/ualberta/cs/shinyexpensetracker/models/GeolocationRequestCode.java
@@ -0,0 +1,10 @@
+package ca.ualberta.cs.shinyexpensetracker.models;
+
+public class GeolocationRequestCode {
+
+	public static final int SET_GEOLOCATION = 1;
+	public static final int DISPLAY_GEOLOCATIONS = 2;
+	
+	private GeolocationRequestCode(){}
+	
+}

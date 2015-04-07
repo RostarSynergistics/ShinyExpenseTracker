cec3755ab4c2e28291e4b82ad309acda7e95194d
Adam Ford <adam.ford@greatbigsolutions.com>
2015-04-06 19:25:18 -0600

    #180: Refactor ElasticSearch-related code out of the Gson persister

---
+++ ShinyExpenseTracker/src/ca/ualberta/cs/shinyexpensetracker/persistence/ElasticSearchExpenseClaimListPersister.java
@@ -0,0 +1,71 @@
+package ca.ualberta.cs.shinyexpensetracker.persistence;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import android.content.Context;
+import ca.ualberta.cs.shinyexpensetracker.es.data.ConnectivityChecker;
+import ca.ualberta.cs.shinyexpensetracker.es.data.ElasticSearchLoad;
+import ca.ualberta.cs.shinyexpensetracker.es.data.ElasticSearchSave;
+import ca.ualberta.cs.shinyexpensetracker.es.data.ElasticSearchSync;
+import ca.ualberta.cs.shinyexpensetracker.models.ExpenseClaimList;
+
+/**
+ * Decorates the GsonExpenseClaimListPersister to maintain the ElasticSearch
+ * data while also maintaining the local copy of the data.
+ */
+public class ElasticSearchExpenseClaimListPersister implements IExpenseClaimListPersister {
+	private final Context context;
+	private final GsonExpenseClaimListPersister childPersister;
+
+	public ElasticSearchExpenseClaimListPersister(Context context, GsonExpenseClaimListPersister childPersister,
+			ConnectivityChecker connectivityChecker) {
+		this.context = context;
+		this.childPersister = childPersister;
+	}
+
+	@Override
+	public ExpenseClaimList loadExpenseClaims() throws IOException {
+
+		ConnectivityChecker checker = new ConnectivityChecker();
+		ElasticSearchLoad load = new ElasticSearchLoad();
+
+		// Creating the two lists to sync if necessary
+		ExpenseClaimList localList = childPersister.loadExpenseClaims();
+
+		// If a network is a available then get the server copy of the claim
+		// list
+		if (checker.isNetworkAvailable(context)) {
+			try {
+				// Loading the data
+				ExpenseClaimList cloudList = load.execute().get();
+
+				// Syncing the two data sources in case of change
+				ElasticSearchSync elasticSearchSync = new ElasticSearchSync(localList, cloudList);
+				localList = elasticSearchSync.sync();
+
+				// Saving the combined list
+				saveExpenseClaims(localList);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return localList;
+
+	}
+
+	@Override
+	public void saveExpenseClaims(ExpenseClaimList list) throws IOException {
+		childPersister.saveExpenseClaims(list);
+
+		ConnectivityChecker checker = new ConnectivityChecker();
+
+		// Saving online if the network is available
+		if (checker.isNetworkAvailable(context)) {
+			new ElasticSearchSave().execute(list);
+		}
+	}
+}
--- ShinyExpenseTracker/src/ca/ualberta/cs/shinyexpensetracker/persistence/GsonExpenseClaimListPersister.java
+++ ShinyExpenseTracker/src/ca/ualberta/cs/shinyexpensetracker/persistence/GsonExpenseClaimListPersister.java
@@ -1,14 +1,7 @@
 package ca.ualberta.cs.shinyexpensetracker.persistence;
 
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 
-import android.content.Context;
-import ca.ualberta.cs.shinyexpensetracker.es.data.ConnectivityChecker;
-import ca.ualberta.cs.shinyexpensetracker.es.data.ElasticSearchLoad;
-import ca.ualberta.cs.shinyexpensetracker.es.data.ElasticSearchSave;
-import ca.ualberta.cs.shinyexpensetracker.es.data.ElasticSearchSync;
-import ca.ualberta.cs.shinyexpensetracker.framework.Application;
 import ca.ualberta.cs.shinyexpensetracker.models.ExpenseClaimList;
 
 import com.google.gson.Gson;
@@ -38,41 +31,15 @@ public class GsonExpenseClaimListPersister implements IExpenseClaimListPersister
 
 	@Override
 	public ExpenseClaimList loadExpenseClaims() throws IOException {
-
 		String travelClaimsListData = persistenceStrategy.load();
-		ConnectivityChecker checker = new ConnectivityChecker();
-		Context context = Application.getAppContext();
-		ElasticSearchLoad load = new ElasticSearchLoad();
 
-		// Creating the two lists to sync if necessary
 		ExpenseClaimList localList;
-		ExpenseClaimList cloudList;
 
 		// Loading local data
 		if (travelClaimsListData.equals("")) {
 			localList = new ExpenseClaimList();
 		} else {
 			localList = gson.fromJson(travelClaimsListData, ExpenseClaimList.class);
-		}
-
-		// If a network is a avialable then get the server copy of the claim
-		// list
-		if (checker.isNetworkAvailable(context)) {
-			try {
-				// Loading the data
-				cloudList = load.execute().get();
-
-				// Syncing the two data sources in case of change
-				ElasticSearchSync elasticSearchSync = new ElasticSearchSync(localList, cloudList);
-				localList = elasticSearchSync.sync();
-
-				// Saving the combined list
-				saveExpenseClaims(localList);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} catch (ExecutionException e) {
-				e.printStackTrace();
-			}
 		}
 
 		return localList;
@@ -83,14 +50,5 @@ public class GsonExpenseClaimListPersister implements IExpenseClaimListPersister
 	public void saveExpenseClaims(ExpenseClaimList list) throws IOException {
 		String travelClaimsString = gson.toJson(list);
 		persistenceStrategy.save(travelClaimsString);
-		ConnectivityChecker checker = new ConnectivityChecker();
-		Context context = Application.getAppContext();
-
-		// Saving online if the network is available
-		if (checker.isNetworkAvailable(context)) {
-			new ElasticSearchSave().execute(list);
-		}
-
 	}
-
 }

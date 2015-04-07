db42b577e7c71c517d6f5126a8609e19d38f79e2
Tristan Meleshko <tmeleshk@ualberta.ca>
2015-04-07 11:34:15 -0600

    Fixed UserType -> Type refactoring

--- ShinyExpenseTracker/src/ca/ualberta/cs/shinyexpensetracker/es/ESClaimManager.java
+++ ShinyExpenseTracker/src/ca/ualberta/cs/shinyexpensetracker/es/ESClaimManager.java
@@ -100,7 +100,7 @@ public class ESClaimManager {
 
 			String json = getEntityContent(response);
 
-			UserType elasticSearchResponseType = new TypeToken<ElasticSearchResponse<ExpenseClaimList>>() {
+			Type elasticSearchResponseType = new TypeToken<ElasticSearchResponse<ExpenseClaimList>>() {
 			}.getType();
 
 			ElasticSearchResponse<ExpenseClaimList> esResponse = gson.fromJson(json, elasticSearchResponseType);
